bba8788071fc847c48dacd011404e15348692ac6
OscarTenesaca <tenesaca.999@gmail.com>
2025-05-01 20:32:47 -0500

    first commit

---
+++ src/config/envs.ts
@@ -0,0 +1,10 @@
+
+// npm i dotenv env-var
+import 'dotenv/config';
+import { get } from 'env-var';
+
+
+export const envs = {
+    PORT: get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
+}
